6fd4483038022191b52932b3ed542dce5f54956a
Vojta Janousek <janousek315@gmail.com>
2019-03-09 11:47:49 +0100

    Added runtime.txt

--- ptvc_project/ptvc_project/settings.py
+++ ptvc_project/ptvc_project/settings.py
@@ -81,11 +81,14 @@ WSGI_APPLICATION = 'ptvc_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
     }
 }
 
+DATABASES['default'] = dj_database_url.config(default="")
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
